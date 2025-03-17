cc55b5f88dd390a42078ec9df4f58b4c6b18834b
geekinryxnn <ryankiratu67@gmail.com>
2025-03-17 10:02:58 +0300

    new changes

--- scripts/index.js
+++ scripts/index.js
@@ -57,15 +57,15 @@ function handleClick(ramen) {
 }
 
 function addSubmitListener() {
-  const form = document.getElementById('new-ramen-form');
+  const form = document.getElementById('ramen-form');
   form.addEventListener('submit', event => {
     event.preventDefault();
     const newRamen = {
-        name: document.getElementById('new-name').value,
-        restaurant: document.getElementById('new-restaurant').value,
-        image: document.getElementById('new-image').value,
-        rating: document.getElementById('new-rating').value,
-        comment: document.getElementById('new-comment').value
+        name: document.getElementById('ramen-name').value,
+        restaurant: document.getElementById('ramen-restaurant').value,
+        image: document.getElementById('ramen-image').value,
+        rating: document.getElementById('ramen-rating').value,
+        comment: document.getElementById('comment').value
     };
     ramens.push(newRamen);
     displayRamens();

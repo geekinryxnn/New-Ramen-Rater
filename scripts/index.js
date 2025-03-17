5984fa7e84c712688c27c8046f1d2bf10f3c2e02
geekinryxnn <ryankiratu67@gmail.com>
2025-03-17 07:59:04 +0300

    first array

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,32 @@ const ramens = [
         rating:  "5", 
         comment: "Delicious!" ,
     },
+
+    const ramens = [
+        { 
+            id: 1, 
+            name: "Shoyu Ramen", 
+            restaurant: "Ichiran", 
+            image: "Images/gyukotsu.jpg", 
+            rating:  "5", 
+            comment: "Delicious!" ,
+        },
+        
+        { 
+            id: 3, 
+            name: "Tonkotsu Ramen", 
+            restaurant: "Ramen-ya", 
+            image: "Images/naruto.jpg", 
+            rating:"8", 
+            comment: "Very colorful",
+        }, 
+
+        { 
+            id: 4 , 
+          name:"Naruto Ramen" ,
+          restaurant:"Kempiski" , 
+          image:"Images/nirvana.jpg" ,
+          rating: "7", 
+          comment:"Very healthy",
+        },
+    ];
